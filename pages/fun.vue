d06ca47e4e9d695d22594c2a92b0c96a49c4d37b
yooseunghun <kinsechs22@gmail.com>
2021-12-13 11:03:15 +0900

    first commit1

---
+++ pages/fun.vue
@@ -0,0 +1,3 @@
+<template>
+<h1> makestar </h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+
+
+ <template>
+   <Tutorial/>
+ </template>
+
+ <script lang="ts">
+ import Vue from 'vue'
+
+ export default Vue.extend({})
+ </script> 
+
+
+<!-- 
+// <template>
+//   <div id="app">
+//     {{ message }}
+//   </div>
+// </template> 
+// <script>
+// var app = new Vue({
+//   el : '#app',
+//   data : {
+//     message: 'hello vue!'
+//   }
+//   })
+
+// </script> -->
+
