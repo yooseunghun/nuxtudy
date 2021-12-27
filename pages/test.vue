0be5ac398684b56b489bf4f49f5e562e7bdb9f82
yooseunghun <kinsechs22@gmail.com>
2021-12-27 17:02:37 +0900

    0001 : init codes for feature

--- pages/test.vue
+++ pages/test.vue
@@ -1,71 +1,58 @@
 <template>
-    <v-card>
-        <v-card-title>
-            V Card Sample
-        </v-card-title>
-        <v-card-text>
-            <v-textarea v-model = "result" />
-        </v-card-text>
-        <v-card-actions>
-            <v-btn @click="onclick">
-                button
-            </v-btn>
-            <v-btn @click="onclickRouter">
-                buttonRouter
-            </v-btn>
-            <v-btn @click="onclickMoment">
-                moment
-            </v-btn>            
-        </v-card-actions>    
-        <v-card-actions>
-            <v-btn @click="onclickDialog">
-                dialog
-            </v-btn>
-            <v-btn @click="onclickNoti">
-                notification
-            </v-btn>                    
-        </v-card-actions>
-        
-    </v-card>
+  <v-card>
+    <v-card-title> V Card Sample feature test </v-card-title>
+    <v-card-text>
+      <v-textarea v-model="result" />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="onclick"> button </v-btn>
+      <v-btn @click="onclickRouter"> buttonRouter </v-btn>
+      <v-btn @click="onclickMoment"> moment </v-btn>
+    </v-card-actions>
+    <v-card-actions>
+      <v-btn @click="onclickDialog"> dialog </v-btn>
+      <v-btn @click="onclickNoti"> notification </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            result: 'result',
-        }
-    },
-    methods: {
-        async onclick() {            
-            const r = await this.$axios.get('/api')
-            this.result = r.data
-        },
-        async onclickRouter() {            
-            const r = await this.$axios.get('/api/hello')
-            this.result = r.data
-        },
-        onclickMoment() {
-            this.result = this.$moment().toLocaleString()            
-        },
-        async onclickDialog () {
-            const res = await this.$dialog.confirm({
-            text: 'Do you really want to exit?',
-            title: 'Warning'
-            })
-        this.result = res
-        },
-        async onclickNoti () {
-            const res = this.$dialog.notify.info('Test notification', {
-                position: 'top-right',
-                timeout: 5000
-            })
-            this.result = res
-        }
+  data() {
+    return {
+      result: 'result',
     }
+  },
+  methods: {
+    async onclick() {
+      const r = await this.$axios.get('/api')
+      this.result = r.data
+    },
+    async onclickRouter() {
+      const r = await this.$axios.get('/api/hello')
+      this.result = r.data
+    },
+    onclickMoment() {
+      this.result = this.$moment().toLocaleString()
+    },
+    async onclickDialog() {
+      const res = await this.$dialog.confirm({
+        text: 'Do you really want to exit?',
+        title: 'Warning',
+      })
+      this.result = res
+    },
+    async onclickNoti() {
+      const res = this.$dialog.notify.info('Test notification', {
+        position: 'top-right',
+        timeout: 5000,
+      })
+      this.result = res
+    },
+  },
 }
 
 //            this.$dialog.notify.info('Test notification', {
-        // position: 'top-right',
-        // timeout: 5000
-        // })
+// position: 'top-right',
+// timeout: 5000
+// })
 </script>
